32b07254b12f5fc75b5a80eaf7062f96a88a4f49
Salvador Guzman <guzmansalv@gmail.com>
2021-03-17 22:54:54 -0700

    add a time dimension to schema

--- schema/02-users.sql
+++ schema/02-users.sql
@@ -3,4 +3,5 @@
 create table duolingo.data.users
 (
 	id bigint not null PRIMARY KEY,
+    at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- schema/03-friends.sql
+++ schema/03-friends.sql
@@ -4,6 +4,7 @@ create table duolingo.data.friends
 (
 	id bigint not null,
 	friend bigint not null,
+    at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT id_ref
       FOREIGN KEY(id) 
 	  REFERENCES duolingo.data.users(id)
